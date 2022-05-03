4d150815f0ee48b9ab6f6085a49be2ccc8e8313a
Rich Ehrhardt <51330294+rich-ehrhardt@users.noreply.github.com>
2022-05-03 14:19:05 +1000

    Update stage1-resource_group.tf

--- test/stages/stage1-resource_group.tf
+++ test/stages/stage1-resource_group.tf
@@ -1,5 +1,5 @@
 module "resource_group" {
-  source = "github.com/ibm-garage-cloud/terraform-azure-resource-group"
+  source = "github.com/cloud-native-toolkit/terraform-azure-resource-group"
 
   resource_group_name = var.resource_group_name
   region              = var.region
